676251ea6996e3149f15a6f89ef9da85e401e9b2
Tad Miller <tad@quantumedge.io>
2017-03-24 16:05:25 -0400

    Restructured repository

--- heartbeat-processor/HeartBeat-Processor.ino
+++ heartbeat-processor/HeartBeat-Processor.ino
@@ -162,3 +162,23 @@ void writeByte(byte myByte)
         digitalWrite(clock, LOW); 
     }
 }
+
+/**
+ * void setup() {
+  Serial.begin(9600);
+  
+  while (!Serial);
+  // wait for serial port to connect
+}
+
+void loop() {
+        char incomingByte;
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                incomingByte = Serial.read();
+                Serial.print("Got: ");
+                Serial.println(incomingByte);
+        }
+        
+}*/
+ */
