1c4c45bf551635c9b73cb9613ec005d4e0903c2c
Danny Nsouli <dmansouli@gmail.com>
2017-03-26 16:06:32 -0400

    adjusting and editing methods of c file, debug

--- commands.c
+++ commands.c
@@ -8,163 +8,30 @@
 #include <termios.h>
 #include <stdint.h>
 
+int fd;
+int count;
+char *device;
+char byte;
+char buf[32];
+
 void resume(){
     printf("you resumed!!\n");
 }
 
-void pause(){
+void pauseProg(){
     printf("you paused!!\n");
 }
 
-void show(){
+void show()
+{
     int x;
    printf("Input what integer you want to be shown: ");
    scanf("%d", &x);
    
 }
 
-void inputCmd(){
-
-    char e[30] = "exit";
-    char r[30] = "resume";
-    char p[30] = "pause";
-    char s[30] = "show";
-    char input[30];
-
-
-    while(1){
-
-        printf("Enter command: ");
-        scanf("%s", input);
-
-        if(strcmp(input,r) == 0){
-
-            resume();
-            inputCmd();
-        
-        }
-        else if(strcmp(input,p) == 0){
-        
-            pause();
-            inputCmd();
-            
-        }
-        else if(strcmp(input,s) == 0){
-
-            show();
-            inputCmd();
-
-        }
-        else if(strcmp(input,e) == 0){
-            
-            break;
-
-        }
-        else{
-
-            printf("Invalid input!\n");
-            inputCmd();
-
-        }
-
-    }
-    printf("You exited the program\n");
-   
-}
-
-int init_tty(int fd);
-
-/**
- * Run this program and pass the location of the serial file descriptor as an argument
- * On linux, this is likely /dev/ttyACM0
- * On OSX, this may be similar to /dev/cu.usbmodem1D1131
- */
-
-int 
-main(int argc, char **argv) {
-    int fd;
-    int count;
-    char *device;
-    char byte;
-    char buf[32];
-
-    /*
-     * Read the device path from input,
-     * or default to /dev/ttyACM0
-     */
-    if (argc == 2) {
-        device = argv[1];
-    } else {
-        //device = "/dev/ttyACM0";
-        device = "/dev/cu.usbmodem1421";
-    }
-    printf("Connecting to %s\n", device);
-
-    /*
-     * Need the following flags to open:
-     * O_RDWR: to read from/write to the devices
-     * O_NOCTTY: Do not become the process's controlling terminal
-     * O_NDELAY: Open the resource in nonblocking mode
-     */
-    fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1) {
-        perror("Error opening serial");
-        return -1;
-    }
-
-    /* Configure settings on the serial port */
-    if (init_tty(fd) == -1) {
-        perror("init");
-        close(fd);
-        return -1;
-    }
-
-    /* Wait for Arduino to reboot */
-    usleep(1000*1000);
-
-    /* Flush whatever is remaining in the buffer */
-    tcflush(fd, TCIFLUSH);
-
-    for (byte = 'a'; byte <= 'z'; byte++) {
-
-        // Write this letter of the alphabet
-        count = write(fd, &byte, 1);
-        if (count == -1) {
-            perror("write");
-            close(fd);
-            return -1;
-        } else if (count == 0) {
-            fprintf(stderr, "No data written\n");
-            close(fd);
-            return -1;
-        }
-
-        // Wait for data to transmit
-        sleep(1);
-
-        // Read the response
-        count = read(fd, &buf, 32);
-        if (count == -1) {
-            perror("read");
-            close(fd);
-            return -1;
-        } else if (count == 0) {
-            fprintf(stderr, "No data returned\n");
-            continue;
-        }
-
-        // Ensure the response is null-terminated
-        buf[count] = 0;
-        printf("(%d): %s", count, buf);
-
-    }
-
-    close(fd);
-    return 0;
-}
-
-int
-init_tty(int fd) {
+int init_tty(int fd)
+{
     struct termios tty;
     /*
      * Configure the serial port.
@@ -172,16 +39,19 @@ init_tty(int fd) {
      * Then, set the baud rate to 9600 (same as on Arduino)
      */
     memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(fd, &tty) == -1) {
+    if (tcgetattr(fd, &tty) == -1)
+    {
         perror("tcgetattr");
         return -1;
     }
 
-    if (cfsetospeed(&tty, (speed_t)B9600) == -1) {
+    if (cfsetospeed(&tty, (speed_t)B9600) == -1)
+    {
         perror("ctsetospeed");
         return -1;
     }
-    if (cfsetispeed(&tty, (speed_t)B9600) == -1) {
+    if (cfsetispeed(&tty, (speed_t)B9600) == -1)
+    {
         perror("ctsetispeed");
         return -1;
     }
@@ -210,7 +80,8 @@ init_tty(int fd) {
     tty.c_cc[VTIME] = 0;
 
     // Update options for the port
-    if (tcsetattr(fd, TCSANOW, &tty) == -1) {
+    if (tcsetattr(fd, TCSANOW, &tty) == -1)
+    {
         perror("tcsetattr");
         return -1;
     }
@@ -218,10 +89,141 @@ init_tty(int fd) {
     return 0;
 }
 
+int sendBytes(char byte)
+{
+      // Write this letter of the alphabet
+        count = write(fd, &byte, 1);
+        if (count == -1) {
+            perror("write");
+            close(fd);
+            return -1;
+        } else if (count == 0) {
+            fprintf(stderr, "No data written\n");
+            close(fd);
+            return -1;
+        }
+
+        // Wait for data to transmit
+        sleep(1);
+
+        // Read the response
+        count = read(fd, &buf, 32);
+        if (count == -1) {
+            perror("read");
+            close(fd);
+            return -1;
+        } else if (count == 0) {
+            fprintf(stderr, "No data returned\n");
+            //continue;
+            return 0;
+        }
+
+        // Ensure the response is null-terminated
+        buf[count] = 0;
+        printf("(%d): %s", count, buf);
+        return 0;
+}
+
+void inputCmd()
+{
+    char e[30] = "exit";
+    char r[30] = "resume";
+    char p[30] = "pause";
+    char s[30] = "show";
+    char input[30];
 
 
+    while(1)
+    {
+        printf("Enter command: ");
+        scanf("%s", input);
 
+        if(strcmp(input, r) == 0){
 
+            resume();
+            inputCmd();
+        
+        }
+        else if(strcmp(input, p) == 0){
+        
+            pauseProg();
+            inputCmd();
+            
+        }
+        else if(strcmp(input, s) == 0){
+
+            show();
+            inputCmd();
+
+        }
+        else if(strcmp(input, e) == 0){
+            break;
+        }
+        else
+        {
+            printf("Invalid input!\n");
+            inputCmd();
+
+        }
+
+    }
+    printf("You exited the program\n");
+   
+}
+
+int connectArduino(int argc, char **argv){
+    /*
+     * Read the device path from input,
+     * or default to /dev/ttyACM0
+     */
+    if (argc == 2)
+        device = argv[1];
+    else
+        device = "/dev/cu.usbmodem1421";
+
+    printf("Connecting to %s\n", device);
+
+    /*
+     * Need the following flags to open:
+     * O_RDWR: to read from/write to the devices
+     * O_NOCTTY: Do not become the process's controlling terminal
+     * O_NDELAY: Open the resource in nonblocking mode
+     */
+    fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror("Error opening serial");
+        return -1;
+    }
+
+    /* Configure settings on the serial port */
+    if (init_tty(fd) == -1) {
+        perror("init");
+        close(fd);
+        return -1;
+    }
+
+    /* Wait for Arduino to reboot */
+    usleep(1000*1000);
+
+    /* Flush whatever is remaining in the buffer */
+    tcflush(fd, TCIFLUSH);
+
+    inputCmd();
+
+    close(fd);
+    return 0;
 }
 
 
+int init_tty(int fd);
+
+/**
+ * Run this program and pass the location of the serial file descriptor as an argument
+ * On linux, this is likely /dev/ttyACM0
+ * On OSX, this may be similar to /dev/cu.usbmodem1D1131
+ */
+
+int main(int argc, char **argv)
+{
+
+}
