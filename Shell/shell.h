84001f661f715f2a28e6c387e450d8bf99aaec66
Tad Miller <tad@quantumedge.io>
2017-04-09 17:02:17 -0400

    Fixed shell code

---
+++ Shell/shell.h
@@ -0,0 +1,45 @@
+/************************************************
+ *              CSCI 3410, Week 10
+ *                  Mini Shell
+ *************************************************/
+
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include "builtins.h"
+
+/* Size to string to tokenize at a time */
+#define TOK_BUFSIZE 16
+
+/* Characters to stop tokenizing on */
+#define TOK_DELIM " \t\r\n\a"
+
+/* Fallback prompt to show */
+#define DEFAULT_PROMPT "> "
+
+static char *builtins[] = {
+        "cd",
+        "exit"
+};
+
+static int(*builtin_funcs[]) (char **) = {
+        &builtin_cd,
+        &builtin_exit
+};
+
+void
+print_prompt(void);
+
+size_t
+read_line(char **);
+
+char **
+tok_line(char *line);
+
+int
+shell_exec(char **args);
+
+int
+shell_run(char **args);
+
+#endif
