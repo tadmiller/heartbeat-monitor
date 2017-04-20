c279dd06119d7a5e5bbc9f047d61afb5f139ae51
Tad Miller <tad@quantumedge.io>
2017-04-20 19:20:25 -0400

    Added csv parser in utils

--- shellv2/utils.c
+++ shellv2/utils.c
@@ -9,220 +9,285 @@
 #include <stdint.h>
 #include <sys/mman.h>
 #include <stdbool.h>
-
+#include <math.h>
 #include "utils.h"
-
-char r_time[8];
 
 // Open and append 10 BPMs to a file
 int mmap_write(int bpms[10], int hours[10], int mins[10], int secs[10])
 {
-    // Our file set
-    const char *filepath = "/tmp/histogram.csv";
+	// Our file set
+	const char *filepath = "/tmp/histogram.csv";
 
-    int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
-    
-    if (fd == -1)
-    {
-        perror("Error opening file for writing");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
+	int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+	
+	if (fd == -1)
+	{
+		perror("Error opening file for writing");
+		return 1;
+		//exit(EXIT_FAILURE);
+	}
 
-    // Stretch the file size to the size of the (mmapped) array of char
+	// Stretch the file size to the size of the (mmapped) array of char
 
-    size_t textsize = 140; // + \0 null character
-    
-    if (lseek(fd, textsize-1, SEEK_SET) == -1)
-    {
-        close(fd);
-        perror("Error calling lseek() to 'stretch' the file");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
-    
-    if (write(fd, "", 1) == -1)
-    {
-        close(fd);
-        perror("Error writing last byte of the file");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
+	size_t textsize = 140; // + \0 null character
+	
+	if (lseek(fd, textsize-1, SEEK_SET) == -1)
+	{
+		close(fd);
+		perror("Error calling lseek() to 'stretch' the file");
+		return 1;
+		//exit(EXIT_FAILURE);
+	}
+	
+	if (write(fd, "", 1) == -1)
+	{
+		close(fd);
+		perror("Error writing last byte of the file");
+		return 1;
+		//exit(EXIT_FAILURE);
+	}
 
-    // Now the file is ready to be mmapped.
-    char *map = mmap(0, textsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED)
-    {
-        close(fd);
-        perror("Error mmapping the file");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
-    
-    int count = 0;
-    char snum[14];
+	// Now the file is ready to be mmapped.
+	char *map = mmap(0, textsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED)
+	{
+		close(fd);
+		perror("Error mmapping the file");
+		return 1;
+		//exit(EXIT_FAILURE);
+	}
+	
+	int count = 0;
+	char snum[14];
 
-    if (bpms == NULL || hours == NULL || mins == NULL || secs == NULL)
-    {
-        fclose(fopen(filepath, "w"));
-    }
-    else
-        for (size_t i = 0; i < 10; i++)
-        {
-            //printf("%.3d,%.2d:%.2d:%.2d,", bpms[i], hours[i], mins[i], secs[i]);
-            sprintf(snum, "%.3d,%.2d:%.2d:%.2d,", bpms[i], hours[i], mins[i], secs[i]);
+	if (bpms == NULL || hours == NULL || mins == NULL || secs == NULL)
+	{
+		fclose(fopen(filepath, "w"));
+	}
+	else
+		for (size_t i = 0; i < 10; i++)
+		{
+			//printf("%.3d,%.2d:%.2d:%.2d,", bpms[i], hours[i], mins[i], secs[i]);
+			sprintf(snum, "%.3d,%.2d:%.2d:%.2d,", bpms[i], hours[i], mins[i], secs[i]);
 
-            if (i < 9)
-                snum[13] = '\n';
+			if (i < 9)
+				snum[13] = '\n';
 
-            //printf("\n%s", snum);
+			//printf("\n%s", snum);
 
-            for (size_t j = count; j < count + 14; j++)
-                map[j] = snum[j % 14];
+			for (size_t j = count; j < count + 14; j++)
+				map[j] = snum[j % 14];
 
-            count += 14;
-        }
+			count += 14;
+		}
 
-    // Write it now to disk
-    if (msync(map, textsize, MS_SYNC) == -1)
-        perror("Could not sync the file to disk");
-    
-    // Don't forget to free the mmapped memory
-    if (munmap(map, textsize) == -1)
-    {
-        close(fd);
-        perror("Error un-mmapping the file");
-        exit(EXIT_FAILURE);
-    }
+	// Write it now to disk
+	if (msync(map, textsize, MS_SYNC) == -1)
+		perror("Could not sync the file to disk");
+	
+	// Don't forget to free the mmapped memory
+	if (munmap(map, textsize) == -1)
+	{
+		close(fd);
+		perror("Error un-mmapping the file");
+		exit(EXIT_FAILURE);
+	}
 
-    // Un-mmaping doesn't close the file, so we still need to do that.
-    close(fd);
+	// Un-mmaping doesn't close the file, so we still need to do that.
+	close(fd);
 
-    return 0;
+	return 0;
 }
 
 void process_hist(char *map)
 {
-    char bpm[4];
-    char time[9];
-    bool selected = true;
-    //bool onbpm = true; // false means on time
+	char r_time[8];
+	char bpm[4];
+	char time[9];
+	bool selected = true;
+	//bool onbpm = true; // false means on time
 
-    if (strcmp(r_time, "-1") == 0)
-        printf("No time selected");
-    else
-        selected = false;
+	if (strcmp(r_time, "-1") == 0)
+		printf("No time selected");
+	else
+		selected = false;
 
-    //printf("%s", map);
+	//printf("%s", map);
 
-    for (size_t i = 0; i < strlen(map); i += 14)
-    {
-        bpm[0] = map[i];
-        bpm[1] = map[i + 1];
-        bpm[2] = map[i + 2];
-        bpm[3] = '\0';
+	for (size_t i = 0; i < strlen(map); i += 14)
+	{
+		bpm[0] = map[i];
+		bpm[1] = map[i + 1];
+		bpm[2] = map[i + 2];
+		bpm[3] = '\0';
 
-        strncpy(time, map + i + 4, 8);
-        time[10] = '\0';
+		strncpy(time, map + i + 4, 8);
+		time[10] = '\0';
 
-        if (strcmp(r_time, time) == 0)
-            selected = true;
+		if (strcmp(r_time, time) == 0)
+			selected = true;
 
-        if (selected == true)
-        {
-            printf("\n%s bpm | %s | ", bpm, time);
+		if (selected == true)
+		{
+			printf("\n%s bpm | %s | ", bpm, time);
 
-            for (size_t j = 0; j < atoi(bpm); j++)
-                printf("*");
-        }
-    }
+			for (size_t j = 0; j < atoi(bpm); j++)
+				printf("*");
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 }
 
-int mmap_read()
+char *mmap_read(char *file)
 {    
-    const char *filepath = "/tmp/histogram.csv";
+	const char *filepath = file != NULL ? file : "/tmp/histogram.csv";
+	const int fd = open(filepath, O_RDONLY, (mode_t) 0600);
 
-    int fd = open(filepath, O_RDONLY, (mode_t)0600);
-    
-    if (fd == -1)
-    {
-        perror("Error opening file for writing");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }        
-    
-    struct stat fileInfo = {0};
-    
-    if (fstat(fd, &fileInfo) == -1)
-    {
-        perror("Error getting the file size");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
-    
-    if (fileInfo.st_size == 0)
-    {
-        fprintf(stderr, "Error: File is empty, nothing to do\n");
-        return 1;
-        //exit(EXIT_FAILURE);
-    }
-    
-    //printf("File size is %ji\n", (intmax_t)fileInfo.st_size);
-    
-    char *map = mmap(0, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED)
-    {
-        close(fd);
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-    
-    // for (off_t i = 0; i < fileInfo.st_size; i++)
-    // {
-    //     printf("Found character %c at %ji\n", map[i], (intmax_t)i);
-    // }
+	printf("\nfile is: %s", filepath);
 
-    process_hist(map);
-    
-    // Don't forget to free the mmapped memory
-    if (munmap(map, fileInfo.st_size) == -1)
-    {
-        close(fd);
-        perror("Error un-mmapping the file");
-        exit(EXIT_FAILURE);
-    }
+	struct stat fileInfo = {0};
+	//char *map;
 
-    // Un-mmaping doesn't close the file, so we still need to do that.
-    close(fd);
-    
-    return 0;
+	if (fd == -1)
+	{
+		perror("Error opening file for writing");
+		return NULL;
+	}
+	
+	if (fstat(fd, &fileInfo) == -1)
+	{
+		perror("Error getting the file size");
+		return NULL;
+	}
+	
+	if (fileInfo.st_size == 0)
+	{
+		fprintf(stderr, "Error: File is empty, nothing to do\n");
+		return NULL;
+	}  //printf("File size is %ji\n", (intmax_t)fileInfo.st_size);
+
+	char *map = mmap(0, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+	if (map == MAP_FAILED)
+	{
+		close(fd);
+		perror("Error mmapping the file");
+		return NULL;
+	}
+
+	// Un-mmaping doesn't close the file, so we still need to do that.
+	close(fd);
+
+	return map;
 }
 
 
 // Print a star histogram using the recorded dates and times.
-void print_histogram(int bpms[10], int hours[10], int mins[10], int secs[10])
+void print_hist(int *bpms, int *hours, int *mins, int *secs)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("\n");
-        
-        printf(hours[i] < 10 ? "0%d" : "%d", hours[i]);
-        printf(mins[i] < 10 ? ":0%d" : ":%d", mins[i]);
-        printf(secs[i] < 10 ? ":0%d" : ":%d", secs[i]);
+	for (i = 0; i < 10; i++)
+	{
+		printf("\n");
+		
+		printf(hours[i] < 10 ? "0%d" : "%d", hours[i]);
+		printf(mins[i] < 10 ? ":0%d" : ":%d", mins[i]);
+		printf(secs[i] < 10 ? ":0%d" : ":%d", secs[i]);
 
-        printf(" | ");
+		printf(" | ");
 
-        for (j = 0; j < bpms[i]; j++)
-            printf("*");
-    }
+		for (j = 0; j < bpms[i]; j++)
+			printf("*");
+	}
+}
+
+// str = map, len = strlen(map)
+// num = bpms/hours/mins/secs
+// i = itr for num
+// j = itr for num - ptr so we can pass it around multiple times
+int parse_commas(char *str, int len, int *numArr, size_t numItr, size_t *j)
+{
+	size_t start = *j;
+	int numSize;
+	char *num;
+
+	while (*j < len && str[*j] != ',')
+	{	// Apparently CR is two characters
+		if (str[*j] == 13)
+			start += 2;
+
+		*j += 1;
+	}
+	if (*j >= len)
+		return 1;
+
+	numSize = *j - start;
+	num = malloc(sizeof(char) * (numSize + 1));
+
+	for (size_t i = 0; i < numSize; i++)
+		num[i] = str[start + i];
+
+	num[numSize + 1] = '\0';
+	numArr[numItr] = atoi(num);
+
+	*j += 1;
+	free(num);
+
+	return 0;
+}
+
+void parse_hist_csv(char *map, int len, int *bpms, int *hours, int *mins, int *secs, int arrLen)
+{
+	size_t j = 0;
+
+	printf("\n%d:", arrLen);
+
+	for (size_t i = 0; i < arrLen; i++)
+	{
+		parse_commas(map, len, bpms, i, &j);
+		parse_commas(map, len, hours, i, &j);
+		parse_commas(map, len, mins, i, &j);
+		parse_commas(map, len, secs, i, &j);
+	}
+}
+
+void process_hist_v2(char **args)
+{
+	char *map = mmap_read(*(args + 1));
+	int len;
+
+	int *bpms;
+	int *hours;
+	int *mins;
+	int *secs;
+	int arrLen;
+
+	if (map == NULL)
+		return;
+
+	len = strlen(map);
+	if (len < 13)
+		return;
+
+	arrLen = len / 15;
+	bpms = malloc(sizeof(int) * arrLen);
+	hours = malloc(sizeof(int) * arrLen);
+	mins = malloc(sizeof(int) * arrLen);
+	secs = malloc(sizeof(int) * arrLen);
+
+	parse_hist_csv(map, len, bpms, hours, mins, secs, arrLen);
+
+	munmap(map, strlen(map));
+	free(bpms);
+	free(hours);
+	free(mins);
+	free(secs);
 }
 
 void clear_mmap()
 {
-    mmap_write(NULL, NULL, NULL, NULL);
+	mmap_write(NULL, NULL, NULL, NULL);
 }
--- shellv2/utils.h
+++ shellv2/utils.h
@@ -5,10 +5,12 @@ void clear_mmap();
 
 void print_histogram(int bpms[10], int hours[10], int mins[10], int secs[10]);
 
-int mmap_read();
+char *mmap_read(char *file);
 
 void process_hist(char *map);
 
 int mmap_write(int bpms[10], int hours[10], int mins[10], int secs[10]);
+
+void process_hist_v2(char **args);
 
 #endif
