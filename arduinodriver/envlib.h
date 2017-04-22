3753b1040cbe7918c00bff8f545f6f35490c2f10
Danny Nsouli <dmansouli@gmail.com>
2017-04-22 17:27:21 -0400

    header file for environment sensor

---
+++ arduinodriver/envlib.h
@@ -0,0 +1,58 @@
+#ifndef _ENVLIB_H
+#define _ENVLIB_H
+
+/*
+BM017_Arduino_color_sensing:  This program interfaces to the AMS TCS34725 color light
+to digital converter IC.  It uses the Arduino I2C interface.  
+
+Schematics associated with the BM017 module may be used for hardware wiring information.
+See the user datasheet at www.solutions-cubed.com for additional information.
+
+*/
+
+#include <Wire.h>
+#include <Math.h>
+
+byte i2cWriteBuffer[10];
+byte i2cReadBuffer[10];
+
+#define SensorAddressWrite 0x29 //
+#define SensorAddressRead 0x29 // 
+#define EnableAddress 0xa0 // register address + command bits
+#define ATimeAddress 0xa1 // register address + command bits
+#define WTimeAddress 0xa3 // register address + command bits
+#define ConfigAddress 0xad // register address + command bits
+#define ControlAddress 0xaf // register address + command bits
+#define IDAddress 0xb2 // register address + command bits
+#define ColorAddress 0xb4 // register address + command bits
+
+/*  
+Send register address and the byte value you want to write the magnetometer and 
+loads the destination register with the value you send
+*/
+void Writei2cRegisters(byte numberbytes, byte command);
+
+/*  
+Send register address to this function and it returns byte value
+for the magnetometer register's contents 
+*/
+byte Readi2cRegisters(int numberbytes, byte command);
+
+void init_TCS34725(void);
+
+void get_TCS34725ID(void);
+
+void init_env();
+
+int get_RedVal();
+
+int get_GreenVal():
+
+int get_BlueVal();
+
+
+
+
+
+
+
