61a136339217de6ead06041f31b18ded5da8fd9a
Tad Miller <tadmill3r@gmail.com>
2017-05-02 23:13:03 -0400

    Added db insert functionality

--- shellv2/db.c
+++ shellv2/db.c
@@ -1,6 +1,7 @@
 #include "db.h"
-#include <sqlite3.h> 
+
 sqlite3 *db;
+int pk;
 
 static int cb(void *NotUsed, int argc, char **argv, char **colName)
 {
@@ -32,18 +33,44 @@ static int cb(void *NotUsed, int argc, char **argv, char **colName)
 
 void db_handler(char **args)
 {
-	
+	if (*(args + 1) == NULL)
+		return;
+
+	if (strcmp(*(args + 1), "init") == 0)
+		db_init();
+	else if (strcmp(*(args + 1), "view") == 0)
+		printf("\n%s\n", *(args + 1));
+	else if (strcmp(*(args + 1), "ins") == 0)
+		db_insert(33, "GREEN");
 }
 
 void db_insert(int rate, char *env)
 {
+	char sql[50];// = "";
+				//"(0, 100, \"Hello\")";
+	sprintf(sql, "INSERT INTO SensorData VALUES(%d, %d, \"%s\")", pk, rate, env);
 
+	printf("\n%s\n", sql);
+	//strcat(sql, pk);
+	//strcat(sql, ", ");
+	//strcat(sql, rate);
+	//strcat(sql, ", \"");
+	//strcat(sql, env);
+	// /strcat(sql, "\");");
+
+	pk++;
+
+	//db_exec(sql);
+}
+
+char *db_select()
+{
+	return NULL;
 }
 
 void db_init()
 {
-	int rex = sqlite3_open("processor.db", &db);
-	char *err;
+
 	char *sql = "CREATE TABLE SensorData"				\
 				"("										\
 					"TIME INT PRIMARY KEY NOT NULL,"	\
@@ -51,7 +78,17 @@ void db_init()
 					"ENV  TEXT NOT NULL"				\
 				");";
 
+	db_exec(sql);
+}
+
+void db_exec(char *sql)
+{
+	int rex = sqlite3_open("processor.db", &db);
+	char *err;
+
 	rex = sqlite3_exec(db, sql, cb, 0, &err);
 
-	printf("%d", rex);
+	printf("\nrex:%d\n", rex);
+	printf("\nerr:%s\n", err);
+	sqlite3_close(db);
 }
--- shellv2/db.h
+++ shellv2/db.h
@@ -3,12 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sqlite3.h> 
+#include <sqlite3.h>
+#include <string.h>
 
 void db_init();
 
 void db_handler(char **args);
 
 void db_insert(int rate, char *env);
+
+void db_exec(char *sql);
+
+char *db_select();
 
 #endif
